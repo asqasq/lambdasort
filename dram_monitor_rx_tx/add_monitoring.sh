3b7139d23b2e07b869a036f61c951ac7dff45b3f
Ana Klimovic <ana.klimov@gmail.com>
2018-04-07 01:53:33 +0000

    Code for monitoring rx/tx on crail-dram datanode

---
+++ dram_monitor_rx_tx/add_monitoring.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+cp StorageServer.java ~/crail/pocket/storage/src/main/java/org/apache/crail/storage/StorageServer.java
+cp TcpStorageServer.java  ~/crail/pocket/storage-narpc/src/main/java/org/apache/crail/storage/tcp/TcpStorageServer.java
